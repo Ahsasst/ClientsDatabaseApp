1732a8f585248306add61dd3a6132ca49ef4147b
Ahsasst <sashko2003@gmail.com>
2025-09-14 18:30:26 +0200

    Initial commit

---
+++ src/main/java/com/projects/ClientsDatabaseApp/ClientsDatabaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.projects.ClientsDatabaseApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsDatabaseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientsDatabaseAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/ClientsDatabaseApp/ClientsDatabaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.ClientsDatabaseApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientsDatabaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
